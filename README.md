11d60c1844224e5b50a8d24ca7f94b0bb73d4085
ilkaay <ilkaay.yuksel@gmail.com>
2021-04-09 03:40:43 +0300

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# graphQL
-vue-apollo-graphQL
---
+++ server.js
@@ -0,0 +1,34 @@
+const { ApolloServer } = require("apollo-server");
+const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
+const resolvers = require("./resolvers");
+
+require("dotenv").config({ path: "variables.env" });
+const User = require("./models/User");
+const Post = require("./models/Post");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: {
+    User,
+    Post,
+  },
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
