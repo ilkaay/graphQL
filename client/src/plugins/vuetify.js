135545aa663ad532dcc86620a7cf914c620bc1dd
ilkaay <ilkaay.yuksel@gmail.com>
2021-04-13 03:40:53 +0300

    Navbar

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,11 +1,32 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  icons: {
-    iconfont: 'md',
+  theme: {
+    dark: false,
+    themes: {
+      light: {
+        secondary: "#607d8b",
+        accent: "#ff5722",
+        error: "#f44336",
+        warning: "#ff9800",
+        info: "#00bcd4",
+        success: "#4caf50",
+        navbar: "#560bad",
+        navbarHover: "#dee2ff",
+      },
+      dark: {
+        primary: "#03a9f4",
+        secondary: "#607d8b",
+        accent: "#e91e63",
+        error: "#f44336",
+        warning: "#ffc107",
+        info: "#00bcd4",
+        success: "#4caf50",
+      },
+    },
   },
 });
