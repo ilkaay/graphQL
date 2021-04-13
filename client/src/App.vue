b9b8bccb6c786e0aaaaa99c5e21e93f02e0422ee
ilkaay <ilkaay.yuksel@gmail.com>
2021-04-13 22:59:43 +0300

    sidebar nav icons

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,12 +28,65 @@
             </button>
           </div>
         </div>
-        <li><router-link to="/posts">Posts</router-link></li>
-        <li><router-link to="/signin">Sign In</router-link></li>
-        <li><router-link to="/signup">Sign Up</router-link></li>
+
+        <!-- Posts -->
+        <li>
+          <router-link to="/posts"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-chat-left-text-fill hidden-md"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"
+              />
+            </svg>
+            <span class="p-2">POSTS</span>
+          </router-link>
+        </li>
+
+        <!-- Sign in -->
+        <li>
+          <router-link to="/signin"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-lock-fill hidden-md"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"
+              />
+            </svg>
+            <span class="p-2">SIGN IN</span>
+          </router-link>
+        </li>
+
+        <!-- Sign up -->
+        <li>
+          <router-link to="/signup">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-pencil-fill hidden-md"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
+              />
+            </svg>
+            <span class="p-2">SIGN UP</span>
+          </router-link>
+        </li>
       </ul>
     </div>
-
     <!-- Horizontal Navbar -->
     <div class="row">
       <div class="col-md-3 p-4">
@@ -199,6 +252,7 @@ export default {
   text-decoration: none;
   background-color: #b8c0ff;
 }
+
 .sidebar-header {
   text-align: center;
   font-size: 20px;
@@ -206,16 +260,19 @@ export default {
   width: 100%;
   display: inline-block;
 }
+
 .sidebar-brand {
   height: 65px;
   position: relative;
   background: #36017c;
   padding-top: 1em;
 }
+
 .sidebar-brand a {
   color: #ddd;
   text-decoration: none;
 }
+
 .hamburger {
   position: absolute;
   left: 0;
@@ -229,6 +286,7 @@ export default {
 #sidebar-wrapper {
   width: 220px;
 }
+
 input {
   background-color: transparent;
   color: #f4f4f4;
