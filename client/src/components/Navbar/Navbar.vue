b8b803a811770da4690aa3d373fb80f18a5bd401
ilkaay <ilkaay.yuksel@gmail.com>
2021-04-18 03:00:52 +0300

    Change Navbar for signed-in user

--- client/src/components/Navbar/Navbar.vue
+++ client/src/components/Navbar/Navbar.vue
@@ -38,12 +38,40 @@
               <span class="d-none d-xl-inline-block m-2">POSTS</span>
             </router-link>
 
-            <router-link to="/signin" class="nav-link text-white m-2">
+            <router-link
+              v-if="user"
+              to="/posts"
+              class="nav-link text-white m-2"
+            >
+              <b-icon-person-circle class="text-white"></b-icon-person-circle>
+              <span class="d-none d-xl-inline-block m-2">PROFILE</span>
+            </router-link>
+
+            <router-link
+              v-if="user"
+              to="/posts"
+              class="nav-link text-white m-2"
+            >
+              <b-icon-box-arrow-right
+                class="text-white"
+              ></b-icon-box-arrow-right>
+              <span class="d-none d-xl-inline-block m-2">LOG OUT</span>
+            </router-link>
+
+            <router-link
+              v-if="!user"
+              to="/signin"
+              class="nav-link text-white m-2"
+            >
               <b-icon-lock-fill class="text-white"></b-icon-lock-fill>
               <span class="d-none d-xl-inline-block m-2">SIGN IN</span>
             </router-link>
 
-            <router-link to="/signup" class="nav-link text-white m-2">
+            <router-link
+              v-if="!user"
+              to="/signup"
+              class="nav-link text-white m-2"
+            >
               <b-icon-pencil-fill class="text-white"></b-icon-pencil-fill>
               <span class="d-none d-xl-inline-block m-2">SIGN UP</span>
             </router-link>
@@ -59,30 +87,64 @@
         class="list-group bg-transparent"
         style="position: relative; top: 10%;"
       >
-        <li class="list-group-item border-0 bg-transparent">
-          <router-link class="text-decoration-none" to="/posts">
-            <b-icon-chat></b-icon-chat>
-            <span class="p-2">POSTS</span>
-          </router-link>
-        </li>
+        <div v-if="user">
+          <li class="list-group-item border-0 bg-transparent">
+            <router-link class="text-decoration-none" to="/posts">
+              <b-icon-chat></b-icon-chat>
+              <span class="p-2">POSTS</span>
+            </router-link>
+          </li>
 
-        <li class="list-group-item border-0 bg-transparent">
-          <router-link class="text-decoration-none" to="/signin">
-            <b-icon-lock-fill></b-icon-lock-fill>
-            <span class="p-2">SIGN IN</span>
-          </router-link>
-        </li>
+          <li class="list-group-item border-0 bg-transparent">
+            <router-link class="text-decoration-none" to="/profile">
+              <b-icon-person-circle></b-icon-person-circle>
+              <span class="p-2">PROFILE</span>
+            </router-link>
+          </li>
 
-        <li class="list-group-item border-0 bg-transparent">
-          <router-link class="text-decoration-none" to="/signup">
-            <b-icon-pencil-fill></b-icon-pencil-fill>
-            <span class="p-2">SIGN UP</span>
-          </router-link>
-        </li>
+          <li class="list-group-item border-0 bg-transparent">
+            <router-link class="text-decoration-none" to="/post/add">
+              <b-icon-star-fill></b-icon-star-fill>
+              <span class="p-2">CREATE POST</span>
+            </router-link>
+          </li>
+
+          <li class="list-group-item border-0 bg-transparent">
+            <router-link class="text-decoration-none" to="/signin">
+              <b-icon-box-arrow-right></b-icon-box-arrow-right>
+              <span class="p-2">LOG OUT</span>
+            </router-link>
+          </li>
+        </div>
+
+        <div v-else>
+          <li class="list-group-item border-0 bg-transparent">
+            <router-link class="text-decoration-none" to="/signin">
+              <b-icon-lock-fill></b-icon-lock-fill>
+              <span class="p-2">SIGN IN</span>
+            </router-link>
+          </li>
+
+          <li class="list-group-item border-0 bg-transparent">
+            <router-link class="text-decoration-none" to="/signup">
+              <b-icon-pencil-fill></b-icon-pencil-fill>
+              <span class="p-2">SIGN UP</span>
+            </router-link>
+          </li>
+        </div>
       </ul>
     </b-sidebar>
   </nav>
 </template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["user"]),
+  },
+};
+</script>
 
 <style scoped>
 .list-group-item {
